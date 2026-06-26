23e0e3ef442c39fa492e8e6b079bdc33388dd06e
MannuDuarte <contatoemanuelleduarte@gmail.com>
2026-06-26 11:04:39 -0300

    Add files via upload

---
+++ livros.js
@@ -0,0 +1,173 @@
+const livros = [
+
+{
+nome:"Harry Potter",
+generos:["Fantasia", "Aventura"],
+tamanho:["Longo"],
+tamanho:["Médio"],   
+protagonista: ["Masculino"],
+publico: ["juvenil"]
+},
+
+{
+nome:"A Seleção",
+generos:["Romance", ],
+tamanho:["Médio"],
+protagonista: ["Feminino"],
+publico: ["Juvenil"]
+},
+
+{
+nome:"Jogos Vorazes",
+generos:["Suspense",],
+tamanho:["Médio"],
+protagonista: ["Diversos"],
+publico: ["Juvenil"]
+
+},
+
+{
+nome:"It - A Coisa",
+generos:["Terror", ],
+tamanho:["Longo"],
+protagonista: ["Masculino"],
+publico: ["Adulto"]
+},
+
+{
+nome:"1984",
+generos:["Distopia", "Ficção Científica", ],
+tamanho:["Médio"],
+protagonista: ["Masculino"],
+publico: ["juvenil"]
+},
+{
+nome:"Os sete maridos de Evelyn Hugo",
+generos:["Romance", ],
+tamanho:["Médio" ],
+protagonista: ["Feminino"],
+publico: ["Adulto", "Juvenil"]
+},
+{
+nome:"O Hobbit",
+generos:["Fantasia", ],
+tamanho:["Médio"],   
+protagonista: ["Masculino"],
+publico: ["infantil"]
+},
+{
+nome:"Rainha Vermelha",
+generos:["Fantasia", "Distopia"],
+tamanho:["Médio"],
+protagonista: ["Feminino"],
+publico: ["juvenil"]
+},
+{nome:"O Conto da Aia",
+generos:["Distopia", "Ficção Científica"],
+tamanho:["Médio"],
+protagonista: ["Feminino"],
+publico: ["Adulto"]
+},
+{nome:"Percy Jackson e o Ladrão de Raios",
+generos:["Fantasia", "Aventura"],
+tamanho:["Médio"],
+protagonista: ["Masculino"],
+publico: ["Infantojuvenil"]
+},
+{nome:"O Senhor dos Anéis",
+generos:["Fantasia", "Aventura"],
+tamanho:["Longo"],  
+protagonista: ["Masculino"],
+publico: ["Adulto", "Juvenil"]
+},
+{nome:"A Menina que Roubava Livros",
+generos:["Drama", "Romance"],
+tamanho:["Médio"],
+protagonista: ["Feminino"],
+publico: ["Juvenil"]
+},
+{nome:"O Diário de Anne Frank",
+generos:["Drama", "Biografia"],
+tamanho:["Médio"],
+protagonista: ["Feminino"],
+publico: ["Juvenil"]
+},  
+{nome:"O Pequeno Príncipe",
+generos:["Fantasia", "Fábula"],
+tamanho:["Curto"],
+protagonista: ["Masculino"],
+publico: ["Infantil"]
+},
+{
+nome:"O Código Da Vinci",
+generos:["Mistério", "Suspense"],
+tamanho:["Médio"],  
+protagonista: ["Masculino"],
+publico: ["Adulto"]
+},  
+{
+nome:"A Culpa é das Estrelas",
+generos:["Romance", "Drama"],
+tamanho:["Médio"],  
+protagonista: ["Feminino"],
+publico: ["Juvenil"]
+},
+{
+    nome:"O sol é para todos",
+    generos:["Drama", "Romance"],
+    tamanho:["Médio"],  
+    protagonista: ["Masculino"],
+    publico: ["Juvenil"]    
+    },
+{
+    nome:"Torto Arado",
+    generos:["Drama", ],
+    tamanho:["Médio"],
+    protagonista: ["Feminino"],
+    publico: ["Adulto"]    
+    },
+    {
+  
+        nome: ["Grande Sertão: Veredas"],
+        generos:["Drama", "Romance"],
+        tamanho:["Longo"],
+        protagonista: ["Masculino"],
+        publico: ["Adulto"]    
+        },
+        {
+            nome:"O Alienista",
+            generos:["Drama", "Mistério"],
+            tamanho:["Médio"],
+            protagonista: ["Masculino"],
+            publico: ["Adulto"]    
+            },
+            {
+                nome: "A hora da estrela",
+                generos:["Drama", "Romance"],
+                tamanho:["Médio"],
+                protagonista: ["Feminino"], 
+                publico: ["Adulto"]
+                },
+                {
+                    nome: "Memórias Póstumas de Brás Cubas",
+                    generos:["Drama", "Romance"],
+                    tamanho:["Médio"],  
+                    protagonista: ["Masculino"],
+                    publico: ["Adulto"]
+                    },
+                    {
+                        nome:["Trono de Vidro"],
+                        generos:["Fantasia", "Aventura"],
+                        tamanho:["Médio"],
+                        protagonista: ["Feminino"],
+                        publico: ["Juvenil"]
+                    },
+                    
+                   
+
+
+                
+                
+                
+
+];
---
+++ script.js
@@ -0,0 +1,60 @@
+function recomendar() {
+
+  const preferencias = [...document.querySelectorAll("input:checked")]
+    .map(i => i.value);
+
+  let livrosPontuados = livros.map(livro => {
+
+    let pontos = 0;
+
+    // gênero
+    livro.generos.forEach(g => {
+      if (preferencias.includes(g)) pontos++;
+    });
+    // tamanho
+    livro.tamanho.forEach(t => {
+      if (preferencias.includes(t)) pontos++;
+    });
+   // protagonista
+    livro.protagonista.forEach(p => {
+      if (preferencias.includes(p)) pontos++;
+    });
+    // público
+    livro.publico.forEach(p => {
+      if (preferencias.includes(p)) pontos++;
+    });
+    // outros campos
+    if (preferencias.includes(livro.tamanho)) pontos++;
+    if (preferencias.includes(livro.protagonista)) pontos++;
+    if (preferencias.includes(livro.publico)) pontos++;
+
+    return { ...livro, pontos };
+  });
+
+  // ordena do maior pro menor
+  livrosPontuados.sort((a, b) => b.pontos - a.pontos);
+
+  let texto = "<h2>Livros recomendados:</h2>";
+
+  let encontrou = false;
+
+  livrosPontuados.forEach(livro => {
+
+    if (livro.pontos > 0) {
+      encontrou = true;
+
+      texto += `
+        <p>
+          📖 ${livro.nome}<br>
+          ${"⭐".repeat(livro.pontos)} (${livro.pontos})
+        </p>
+      `;
+    }
+  });
+
+  if (!encontrou) {
+    texto += "<p>Nenhum livro encontrado.</p>";
+  }
+
+  document.getElementById("resultado").innerHTML = texto;
+}
